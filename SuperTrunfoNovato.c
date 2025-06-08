6df827b10baf02d5086f660acd175018b4a7ff03
Samara Chaves <samarachaves1999@hotmail.com>
2025-06-07 23:36:38 -0300

    SuperTrunfoNovato.c
    Editei e corrigi alguns problemas

--- SuperTrunfoNovato.c
+++ SuperTrunfoNovato.c
@@ -5,12 +5,14 @@ int main() {
   char codigo2[4] = "B2", nome2[50] = "Londrina";
   int populacao1 = 418608, populacao2 = 577318;
   int pontos1 = 10, pontos2 = 80;
-  float area1 = 280.674, area2 = 1.652.569;
-  float pib1 = 22.073.535, 23.604.749;
+  float area1 = 280674, area2 = 1652569;
+  float pib1 = 22073535, pib2 = 23604749;
 
   printf("\n--- Cidade 1 ---\n");
   printf("Código: %s\nNome %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos Turísticos: %d\n",
     codigo1, nome1, populacao1, area1, pontos1);
+
+  printf("\n--- Cidade 2 ---\n");
   printf("Código: %s\nNome %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos Turísticos: %d\n",
     codigo2, nome2, populacao2, area2, pontos2);
 
