01a8df4960ab61c488b08f82d2a9462c342c65b5
pmorefiction <samarachaves1999@hotmail.com>
2025-06-07 22:02:19 -0300

    SuperTrunfoNovato.c
    Adicionei o "int" e "float"

--- SuperTrunfoNovato.c
+++ SuperTrunfoNovato.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
 int main() {
-char codigo1[4] = "A1", nome1[50] = "Santos";
-char codigo2[4] = "B2", nome2[50] = "Londrina";
+  char codigo1[4] = "A1", nome1[50] = "Santos";
+  char codigo2[4] = "B2", nome2[50] = "Londrina";
+  int populacao1 = 418608, populacao2 = 577318;
+  int pontos1 = 10, pontos2 = 80;
+  float area1 = 280.674, area2 = 1.652.569;
+  float pib1 = 22.073.535, 23.604.749;
